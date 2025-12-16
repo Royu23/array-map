f622b06568db9434e941fa37453c8eb6fa8b01bf
Your Name <your-email@example.com>
2025-12-16 16:29:07 +0530

    filtered and displayed

--- map.js
+++ map.js
@@ -1,124 +1,144 @@
-const display=[
 
-
-[
+const display=
+  
+  [
   {
     "albumId": 1,
     "id": 1,
-    "title": "accusamus beatae ad facilis cum similique qui sunt",
-    "url": "https://via.placeholder.com/600/92c952",
-    "thumbnailUrl": "https://via.placeholder.com/150/92c952"
+    "title": "mango",
+    "url": "https://picsum.photos/seed/1/600",
+    "thumbnailUrl": "https://picsum.photos/seed/1/150"
   },
   {
     "albumId": 1,
     "id": 2,
-    "title": "reprehenderit est deserunt velit ipsam",
-    "url": "https://via.placeholder.com/600/771796",
-    "thumbnailUrl": "https://via.placeholder.com/150/771796"
+    "title": "apple",
+    "url": "https://picsum.photos/seed/2/600",
+    "thumbnailUrl": "https://picsum.photos/seed/2/150"
   },
   {
     "albumId": 1,
     "id": 3,
-    "title": "officia porro iure quia iusto qui ipsa ut modi",
-    "url": "https://via.placeholder.com/600/24f355",
-    "thumbnailUrl": "https://via.placeholder.com/150/24f355"
+    "title": "potato",
+    "url": "https://picsum.photos/seed/3/600",
+    "thumbnailUrl": "https://picsum.photos/seed/3/150"
   },
   {
     "albumId": 1,
     "id": 4,
     "title": "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
-    "url": "https://via.placeholder.com/600/d32776",
-    "thumbnailUrl": "https://via.placeholder.com/150/d32776"
+    "url": "https://picsum.photos/seed/4/600",
+    "thumbnailUrl": "https://picsum.photos/seed/4/150"
   },
   {
     "albumId": 1,
     "id": 5,
     "title": "natus nisi omnis corporis facere molestiae rerum in",
-    "url": "https://via.placeholder.com/600/f66b97",
-    "thumbnailUrl": "https://via.placeholder.com/150/f66b97"
+    "url": "https://picsum.photos/seed/5/600",
+    "thumbnailUrl": "https://picsum.photos/seed/5/150"
   },
   {
     "albumId": 1,
     "id": 6,
     "title": "accusamus ea aliquid et amet sequi nemo",
-    "url": "https://via.placeholder.com/600/56a8c2",
-    "thumbnailUrl": "https://via.placeholder.com/150/56a8c2"
+    "url": "https://picsum.photos/seed/6/600",
+    "thumbnailUrl": "https://picsum.photos/seed/6/150"
   },
   {
     "albumId": 1,
     "id": 7,
     "title": "officia delectus consequatur vero aut veniam explicabo molestias",
-    "url": "https://via.placeholder.com/600/b0f7cc",
-    "thumbnailUrl": "https://via.placeholder.com/150/b0f7cc"
+    "url": "https://picsum.photos/seed/7/600",
+    "thumbnailUrl": "https://picsum.photos/seed/7/150"
   },
   {
     "albumId": 1,
     "id": 8,
     "title": "aut porro officiis laborum odit ea laudantium corporis",
-    "url": "https://via.placeholder.com/600/54176f",
-    "thumbnailUrl": "https://via.placeholder.com/150/54176f"
+    "url": "https://picsum.photos/seed/8/600",
+    "thumbnailUrl": "https://picsum.photos/seed/8/150"
   },
   {
     "albumId": 1,
     "id": 9,
     "title": "qui eius qui autem sed",
-    "url": "https://via.placeholder.com/600/51aa97",
-    "thumbnailUrl": "https://via.placeholder.com/150/51aa97"
+    "url": "https://picsum.photos/seed/9/600",
+    "thumbnailUrl": "https://picsum.photos/seed/9/150"
   },
   {
     "albumId": 1,
     "id": 10,
     "title": "beatae et provident et ut vel",
-    "url": "https://via.placeholder.com/600/810b14",
-    "thumbnailUrl": "https://via.placeholder.com/150/810b14"
+    "url": "https://picsum.photos/seed/10/600",
+    "thumbnailUrl": "https://picsum.photos/seed/10/150"
   },
   {
     "albumId": 1,
     "id": 11,
     "title": "nihil at amet non hic quia qui",
-    "url": "https://via.placeholder.com/600/1ee8a4",
-    "thumbnailUrl": "https://via.placeholder.com/150/1ee8a4"
+    "url": "https://picsum.photos/seed/11/600",
+    "thumbnailUrl": "https://picsum.photos/seed/11/150"
   },
   {
     "albumId": 1,
     "id": 12,
     "title": "mollitia soluta ut rerum eos aliquam consequatur perspiciatis maiores",
-    "url": "https://via.placeholder.com/600/66b7d2",
-    "thumbnailUrl": "https://via.placeholder.com/150/66b7d2"
+    "url": "https://picsum.photos/seed/12/600",
+    "thumbnailUrl": "https://picsum.photos/seed/12/150"
   },
   {
     "albumId": 1,
     "id": 13,
     "title": "repudiandae iusto deleniti rerum",
-    "url": "https://via.placeholder.com/600/197d29",
-    "thumbnailUrl": "https://via.placeholder.com/150/197d29"
+    "url": "https://picsum.photos/seed/13/600",
+    "thumbnailUrl": "https://picsum.photos/seed/13/150"
   },
   {
     "albumId": 1,
     "id": 14,
     "title": "est necessitatibus architecto ut laborum",
-    "url": "https://via.placeholder.com/600/61a65",
-    "thumbnailUrl": "https://via.placeholder.com/150/61a65"
+    "url": "https://picsum.photos/seed/14/600",
+    "thumbnailUrl": "https://picsum.photos/seed/14/150"
   },
   {
     "albumId": 1,
     "id": 15,
     "title": "harum dicta similique quis dolore earum ex qui",
-    "url": "https://via.placeholder.com/600/f9cee5",
-    "thumbnailUrl": "https://via.placeholder.com/150/f9cee5"
+    "url": "https://picsum.photos/seed/15/600",
+    "thumbnailUrl": "https://picsum.photos/seed/15/150"
   }
 ]
-];
+
+    
+   
 
 
-const fill=display.map(items=>{
-  return
+   const list=document.getElementById("list").innerHTML=display.map(item=>{
+    return`
+    
+      <h1>${item.title}</h1>
+      <img src=${item.url}>
+      `
+   ;}).join();
+   const  inputval=document.getElementById("search")
+  const answer=inputval.addEventListener('input',function(e){
+    const result=e.target.value.toLowerCase();
+    const name=display.filter(item=>{
+      return item.title.includes(result);
+    });
+    name.length!=0?
+    document.getElementById("list").innerHTML=name.map(item=>{
+      return`
+       <h1>${item.title}</h1>
+      <img src=${item.url}>
+      `
+    ;}).join():alert( "not found");
+    
+  }
+  )
 
-<div key={"items.id"}>
-  <a href="${items?.url}" >url</a>
-  <h1>${"items?.title"}</h1>
-  <a href="${items?.thumbnailUrl}">thumbnailUrl</a>
-  </div>
-  ;}).join("");
+  
 
-document.getElementById("royu").innerHTML=fill;
+
+
+
